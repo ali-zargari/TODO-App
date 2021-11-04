029d7835aa4d3658ecb1072c60292fdd0ac92c39
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2021-11-04 14:13:27 -0700

    Skeleton is almost fully polished.
    Added the elements for a TODO card.

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,22 @@
 import {init, modifyContainer, addElement, generateGrid} from "./tools.js";
 
 let currentTodo = '';
+let size = 0;
+
+let todo_list = [];
+
+let Todo = function Todo (ed, td){
+    let end_date = ed;
+    let todo = td;
+    let done = false;
+    let priority = 0;
+};
+
+
 
 function m(){
     //document.querySelector('.task'+'').addEventListener('')
-    document.getElementsByClassName('task')[0].addEventListener('change', function (e){
+    document.getElementsByClassName('todo')[0].addEventListener('change', function (e){
         saveText(e);
     });
 
@@ -15,15 +27,17 @@ function m(){
 
 let saveText = (e) =>{
     addElement('div','entry',e.srcElement);
-    currentTodo = document.getElementsByClassName('task'+'').item(0).cloneNode(true);
+    currentTodo = document.getElementsByClassName('todo'+'').item(0).cloneNode(true);
 };
 
 
 let addEntry = () => {
     console.log('here');
     let list = document.querySelector('.list'+'');
-    addElement('div','entry',list);
-    list.appendChild(currentTodo);
+    let newEntry = addElement('div','entry',list);
+    newEntry.innerHTML = currentTodo.value;
+    newEntry.id = size;
+    size++;
 };
 
 window.saveText= saveText;
